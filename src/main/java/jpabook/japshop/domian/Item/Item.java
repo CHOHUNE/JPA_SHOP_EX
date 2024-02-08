07ad3cfeb8c930976389ed3c27f3512aa98055eb
CHOHUNE <yuureru@gmail.com>
2024-02-08 12:09:52 +0900

    2/8 상품 엔티티 개발 ( 비즈니스 로직 추가 - 재고 추가, 재고 삭제, 재고 예외 )

--- src/main/java/jpabook/japshop/domian/Item/Item.java
+++ src/main/java/jpabook/japshop/domian/Item/Item.java
@@ -2,6 +2,7 @@ package jpabook.japshop.domian.Item;
 
 import jakarta.persistence.*;
 import jpabook.japshop.domian.Category;
+import jpabook.japshop.domian.exception.NotEnoughStockException;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -27,5 +28,18 @@ public abstract class Item {
     private List<Category> categories = new ArrayList<>();
 
 
+//    비즈니스 로직 - 엔티티 안에서 비즈니스 로직을 작성함 - 객체지향적..
+//    getter로 가지고 오고 더하고 빼고 다시 set 하고 하는게 아니라
+//    데이터를 가지고 있는 쪽에 비즈니스 메서드를 작성 하는게 객체의 응집력이 있음.. MSA 구조와 비슷?...
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
 
+    public void removeStock(int quantity) throws NotEnoughStockException {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("nedd more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
